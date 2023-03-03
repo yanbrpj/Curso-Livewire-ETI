b28e7c1a3a960c5bd57a996d416482d5d3dfee60
yanbrpj <inteelbr@hotmail.com>
2023-03-03 12:00:19 -0300

    06 - Listar Registros

--- resources/views/livewire/show-tweets.blade.php
+++ resources/views/livewire/show-tweets.blade.php
@@ -3,4 +3,12 @@
     {{ $message }} <br>
 
     <input type="text" name="message" id="message" value="" wire:model="message">
+
+    <br>
+    <br>
+    <ul>
+        @foreach ($tweets as $tweet)
+            <li>{{ $tweet->user->name }} - {{ $tweet->content }}</li>
+        @endforeach
+    </ul>
 </div>
