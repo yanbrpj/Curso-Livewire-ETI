d2720d5dec71a7c6d59b1a27e82a2fb0fe8c2519
yanbrpj <inteelbr@hotmail.com>
2023-03-03 11:52:40 -0300

    05 - Criar Componentes e Data Binding

---
+++ resources/views/livewire/show-tweets.blade.php
@@ -0,0 +1,6 @@
+<div>
+    Show Tweets <br>
+    {{ $message }} <br>
+
+    <input type="text" name="message" id="message" value="" wire:model="message">
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,9 @@
 <?php
 
+use App\Http\Livewire\ShowTweets;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('tweets', ShowTweets::class);
 
 Route::get('/', function () {
     return view('welcome');
